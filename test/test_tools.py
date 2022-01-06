8c60019f067a3974b94fa9327c1e6ede20a0193d
Russell Klopfer <russell@kloper.us>
2022-01-06 18:32:38 -0500

    fix tests

--- test/test_tools.py
+++ test/test_tools.py
@@ -1,13 +1,16 @@
+from find_raspi import net
+
+
 def test_get_network():
-    network = tools.get_network()
+    network = net.get_network()
     assert network
 
 
 def test_find_devices():
-    devices = tools.find_sshable()
+    devices = net.find_sshable()
     assert devices
 
 
 def test_find_pi():
-    pi_addr = tools.find_pis()
+    pi_addr = net.find_pis()
     print(pi_addr)
