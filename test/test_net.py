e8769cc184f13d3a76d48ad31c1b71673183d8cf
Russell Klopfer <russell@kloper.us>
2022-01-09 03:14:05 -0500

    clean up

--- test/test_net.py
+++ test/test_net.py
@@ -10,12 +10,3 @@ def test_find_sshable():
     sshable = net.find_sshable(host_timeout="1")
     print(f"ssh-able devices: {sshable}")
     assert sshable is not None
-
-
-def test_do():
-    res = net.is_sshable("192.168.86.20")
-    assert res
-    res = net.is_sshable("192.168.86.21")
-    assert not res
-    res = net.is_sshable("192.168.86.26")
-    assert not res
