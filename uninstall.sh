d9822f452806ee7a39ab112b822c5fc944f40d85
Russell Klopfer <russell@kloper.us>
2022-01-13 21:01:06 -0500

    clean up install scripts

--- uninstall.sh
+++ uninstall.sh
@@ -1,9 +1,35 @@
 #!/usr/bin/env bash
 set -e
 
-BIN_DIR=$HOME/bin
-VENV_DIR=$BIN_DIR/.find-sshable 
+
+usage() { 
+echo "Usage: $0 [-h] [<install_dir>]
+<install_dir> defaults to $HOME/bin
+-h :: display this message" 2>&1
+}
+
+while getopts "h" Option; do
+    case "${Option}" in
+        h) 
+            usage
+            exit 0
+            ;;
+        *)
+            usage
+            exit 1
+            ;;
+    esac
+done
+shift $((OPTIND-1))
+
+# override INSTALL_LOC with first arg
+INSTALL_LOC=$HOME/bin
+if [[ $1 ]]; then
+    INSTALL_LOC="$1"
+fi
+
+VENV_DIR=$INSTALL_LOC/.find-sshable 
 
 
-rm -f $BIN_DIR/find-sshable
+rm -f $INSTALL_LOC/find-sshable
 rm -rf $VENV_DIR
